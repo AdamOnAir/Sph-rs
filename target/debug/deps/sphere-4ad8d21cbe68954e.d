866f056edca95f9d0d45a59bb57005d5ef121fbf
FBDev64 <elzadam11@gmail.com>
2024-05-01 17:42:15 +0000

    Super...

---
+++ target/debug/deps/sphere-4ad8d21cbe68954e.d
@@ -0,0 +1,5 @@
+/home/fbdev/dev/rust/sphere/target/debug/deps/sphere-4ad8d21cbe68954e: src/main.rs
+
+/home/fbdev/dev/rust/sphere/target/debug/deps/sphere-4ad8d21cbe68954e.d: src/main.rs
+
+src/main.rs:
